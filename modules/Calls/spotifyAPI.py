63ebd667779a4b1bf70d46be43d502805afe66d1
anim1311 <65235028+anim1311@users.noreply.github.com>
2022-01-25 22:14:45 +0100

    updated the import function of setting.py

--- modules/Calls/spotifyAPI.py
+++ modules/Calls/spotifyAPI.py
@@ -4,11 +4,8 @@ import spotipy
 import json
 import os
 #Getting the Api codes from a file
-#from ...settings import *
+from ...settings import *
 
-spotifyClientSecret = "f8c4266d96054becbd8d2bc78f0f0267"
-spotifyClientID = "b1170fe77878496ba538a2ec71c8f196"
-spotifyUsername = "iq8iheoou0yxlyrdt5hss1sc7"
 username =spotifyUsername
 clientID = spotifyClientID
 clientSecret =spotifyClientSecret
